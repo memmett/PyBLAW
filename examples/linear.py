3ce75e0ff1634180e17e528a2ebd4a32cf2fd6ef
Matthew Emmett <matthew@emmett.ca>
2009-10-17 12:31:53 -0600

    Use new WENO reconstructor.

--- examples/linear.py
+++ examples/linear.py
@@ -9,22 +9,11 @@
 
    where A and B are constant (and possibly singular) matrices.
 
-   NOTE: This is a pure python implementation and is **VERY** slow.
-   To speed this up, one should
-
-     * cache the grid and recontruction matrices;
-     * avoid calculating the same thing twice; and
-     * use a hybrid approach (ie, write the flux and source functions in C).
-
 """
 
 import math
-import cProfile as profile
-import pstats
 
-import h5py as h5
 import numpy as np
-import scipy.linalg
 import scipy.sparse
 
 import pyblaw.system
@@ -37,7 +26,15 @@ import pyblaw.solver
 import pyweno.stencil
 
 
-def initial_condition(x, t):
+######################################################################
+# initial condition and solver
+#
+
+def q0(x, t):
+    """Initial condition.
+
+       XXX
+    """
 
     if x < -t or x > t:
         return np.zeros(3)
@@ -45,116 +42,20 @@ def initial_condition(x, t):
     return np.array([ math.cos(0.5*math.pi*x/t), 0.0, 0.0 ])
 
 
-
-######################################################################
-# reconstructor
-#
-
-# XXX: move this to pyblaw
-
-class PolynomialReconstructor(pyblaw.reconstructor.Reconstructor):
-    """Polynomial reconstructor.
-
-       The unknown q is recontructed at the cell boundaries and
-       quadrature points using a high order polynomial interpolation.
-
-       To use: don't do anything (or, adjust matrices to handle your
-               boundary conditions appropriately).
-
-       NOTE: If your solution has a discontinuity, this recontructor
-             will do a very poor job of recontructing.  Try using a
-             WENO recontructor instead.
+class LinearSolver(pyblaw.solver.Solver):
+    """Solver for a linear system.
 
     """
 
-    n = 3                               # number of quadrature points
+    def __init__(self, A, B, boundaries, times, order=4):
 
-    def __init__(self, order):
-        self.k = order
+        p = A.shape[0]
 
-
-    def allocate(self):
-
-        N = self.grid.N
-        p = self.system.p
-        k = self.k
-        n = self.n
-
-        print "building reconstructors..."
-
-        # compute reconstruction coeffs
-        r = (k / 2) + (k % 2)
-        stencil = pyweno.stencil.Stencil(order=k, shift=r, quad=n, grid=self.grid)
-
-        # build reconstructor matrix
-        BNDRYL = scipy.sparse.lil_matrix((N,N))
-        BNDRYR = scipy.sparse.lil_matrix((N,N))
-        QUAD   = scipy.sparse.lil_matrix((N*n,N))
-
-        # boundary condition at left of domain
-        for i in xrange(2*k):
-            BNDRYL[i,i] = 1.0
-            BNDRYR[i,i] = 1.0
-            for l in xrange(n):
-                QUAD[i+l,i] = 1.0
-
-        # middle of domain: don't do anything here
-        for i in xrange(2*k, N-2*k):
-            BNDRYL[i,i-1-r:i-1-r+k] = stencil.c_p[i-1,:]
-            BNDRYR[i,i-r:i-r+k]     = stencil.c_m[i,:]
-            for l in xrange(n):
-                QUAD[i*n+l,i-r:i-r+k] = stencil.c_q[i,l,:]
-
-        # boundary condition at right of domain
-        for i in xrange(N-2*k, N):
-            BNDRYL[i,i] = 1.0
-            BNDRYR[i,i] = 1.0
-            for l in xrange(n):
-                QUAD[i+l,i] = 1.0
-
-        self.BNDRYL = BNDRYL.tocsr()
-        self.BNDRYR = BNDRYR.tocsr()
-        self.QUAD   = QUAD.tocsr()
-
-        print "building reconstructors... done."
-
-
-    def reconstruct(self, q, ql, qr, qq):
-
-        p = self.system.p
-
-        # XXX: use weno instead of central polynomial...
-
-        for m in xrange(p):
-            ql[:,m] = self.BNDRYL * q[:,m]
-            qr[:,m] = self.BNDRYR * q[:,m]
-            qq[:,m] = self.QUAD * q[:,m]
-
-
-
-######################################################################
-# solver
-#
-
-# XXX: move this to pyblaw
-
-class ExampleLinearSolver(pyblaw.solver.Solver):
-    """Example solver for a linear system.
-
-       To use: tweak if you need to...
-
-    """
-
-    A = np.matrix('2.0 0.0 0.0; 0.0 -1.0 0.0; 0.0 0.0 -2.0')
-    B = np.matrix('0.0 0.0 0.0; 0.0 0.0 0.0; 0.0 -0.1 0.1')
-
-    def __init__(self, order=8, cell_boundaries=None, times=None):
-
-        grid          = pyblaw.grid.Grid(boundaries=cell_boundaries)
-        system        = pyblaw.system.LinearSystem(self.A, self.B, initial_condition)
-        reconstructor = PolynomialReconstructor(order)
-        flux          = pyblaw.flux.LinearLFFlux(system.A)
-        source        = pyblaw.source.LinearQuad3Source(system.B)
+        grid          = pyblaw.grid.Grid(boundaries=boundaries)
+        system        = pyblaw.system.SimpleSystem(p, q0, parameters={'A': A, 'B': B})
+        reconstructor = pyblaw.reconstructor.WENOReconstructor(order, 3, 'cache.h5')
+        flux          = pyblaw.flux.LinearLFFlux(A)
+        source        = pyblaw.source.LinearQuad3Source(B)
         evolver       = pyblaw.evolver.SSPERK3()
         dumper        = pyblaw.dumper.H5PYDumper('output.h5')
 
@@ -182,14 +83,14 @@ class ExampleLinearSolver(pyblaw.solver.Solver):
 # giv'r!
 #
 
-k = 8
+k = 3
+
+A = np.matrix('2.0 0.0 0.0; 0.0 -1.0 0.0; 0.0 0.0 -2.0')
+B = np.matrix('0.0 0.0 0.0; 0.0 0.0 0.0; 0.0 -0.1 0.1')
+
 x = np.linspace(-400.0, 400.0, 1600+1)
 t = np.linspace(100.0,  110.0, 100+1)
 
-solver = ExampleLinearSolver(order=k,
-                             cell_boundaries=x,
-                             times=t)
+solver = LinearSolver(A, B, x, t, order=k)
 
-profile.run('solver.run()', 'linear.prof')
-p = pstats.Stats('linear.prof')
-p.strip_dirs().sort_stats('time', 'cum').print_stats(10)
+solver.run()
