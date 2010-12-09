b632c77e7de03e4d8e0d02c24f21711689e534aa
Matthew Emmett <matthew@emmett.ca>
2010-12-08 19:50:31 -0500

    PyBLAW release v0.5.10.

--- pyblaw/mingw.py
+++ pyblaw/mingw.py
@@ -14,9 +14,9 @@ if os.name == 'nt':
 
     # XXX: we're assuming that MinGW is installed in C:\MinGW (default)
     if os.environ.has_key('PATH'):
-        os.environ['PATH'] = os.environ['PATH'] + ';C:\MinGW\bin'
+        os.environ['PATH'] = os.environ['PATH'] + r';C:\MinGW\bin'
     else:
-        os.environ['PATH'] = 'C:\MinGW\bin'
+        os.environ['PATH'] = r'C:\MinGW\bin'
 
     mingw_setup_args = { 'options': { 'build_ext': { 'compiler': 'mingw32' } } }
     pyximport.install(setup_args=mingw_setup_args)
